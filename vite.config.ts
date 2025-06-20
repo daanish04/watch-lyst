551e4495940791cbbc56766dce0dd2f807b18502
daanish04 <108368229+daanish04@users.noreply.github.com>
2025-06-20 17:25:46 +0530

    fix(vercel): set base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     proxy: {
       // Proxy requests from /api to your local backend server
